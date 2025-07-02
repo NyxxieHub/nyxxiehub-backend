57b498d0e216dea45d4d5ae35514f1773ca039f5
Maria Rita Boareto <mariaritaboareto@gmail.com>
2025-07-02 19:45:59 -0300

    test without the requireAuth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { facebookCallback } from "../controllers/auth/facebook-callback";
 import { login } from "../controllers/auth/login";
-import { requireAuth } from "../middleware/require-auth";
 
 const router = express.Router();
 
 router.post("/auth/login", login);
-router.get("/auth/facebook/callback", requireAuth, facebookCallback);
+router.get("/auth/facebook/callback", facebookCallback);
 
 export default router;
